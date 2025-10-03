f265909ee3d23964fb74445b5ea0b3562225c11e
Maniacollas <maniacollas@gmail.com>
2025-10-03 10:47:01 -0300

    Adicionado objetos

--- src/br/com/etec/biblioteca/cadastro/ClassePrincipal.java
+++ src/br/com/etec/biblioteca/cadastro/ClassePrincipal.java
@@ -4,4 +4,13 @@ public class ClassePrincipal {
 
 	public static void main(String[] args) {
 	
+		Infomacoes Info_obj = new Infomacoes();
+		Infomacoes Info_obj2 = new Infomacoes
+				("Dom Casmurro", "Machado de Assis");
+				
+		Info_obj.mostrarDados();
+		
+		Info_obj2.mostrarDados();
+				
+}
 }
--- src/br/com/etec/biblioteca/cadastro/Infomacoes.java
+++ src/br/com/etec/biblioteca/cadastro/Infomacoes.java
@@ -12,9 +12,48 @@ public class Infomacoes {
 	public Infomacoes(){
 }
 
-public Infomacoes(String autor, String titulo){
-	this.autoresAt = autor;
-	this.titulosAt = titulo;
+	public Infomacoes(String autor, String titulo){
+		this.autoresAt = autor;
+		this.titulosAt = titulo;
 }
 
-}
+	public String getTitulos(){
+		return titulosAt;
+	}
+	public String getSubitulos(){
+		return subtitulosAt;
+	}
+	public String getAutoress(){
+		return autoresAt;
+	}
+	public String getGeneros(){
+		return generosAt;
+	}
+	public int getNumerosPaginas(){
+		return numerosPaginasAt;
+	}
+	public void setAutores(String name){
+		this.autoresAt = name;
+	}
+	public void setTitulos(String title){
+		this.titulosAt = title;
+	}
+	public void setSubtitulos(String subtitles){
+		this.subtitulosAt = subtitles;
+	}
+	public void setgeneros(String gender){
+		this.generosAt = gender;
+	}
+	public void setNumerosPaginas(int page){
+		this.numerosPaginasAt = page;
+	}
+	public void mostrarDados(){
+		System.out.println("Informações do Produto:");
+		System.out.println(titulosAt);
+		System.out.println(subtitulosAt);
+		System.out.println(autoresAt);
+		System.out.println(generosAt);
+		System.out.println(numerosPaginasAt);
+		
+	}
+	}
